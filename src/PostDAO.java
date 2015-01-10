ac7f07b85d85683167e12e2068336fad8989f2ae
necrotus <ragnahr.asd@gmx.de>
2015-01-10 12:40:32 +0100

    refactor to DAO

---
+++ src/PostDAO.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Date;
+
+public class PostDAO {
+	
+	int id ;
+	String Ersteller;
+	Date Erstelldatum;
+	String text;
+
+	
+	
+	PostDAO(){
+		
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+}
