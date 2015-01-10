30705100d7d7447f3ffec683ba5576ac698b434f
Judith Rasehorn <corbit@arcor.de>
2015-01-10 14:21:26 +0100

    Forum.java

--- src/Forum.java
+++ src/Forum.java
@@ -3,14 +3,22 @@ public class Forum {
 
 	private int id;
 	private String name;
-	private ArrayList<Thread> threadsList;
+	private ArrayList<Thread> threadList;
 
 	public Forum(String name) {
 		this.name = name;
+		this.threadList = new ArrayList<Thread>();
 	}
 
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	public void addThread(Thread t){
+		threadList.add(t);
+		
+	}
+	
+	
 
 }
