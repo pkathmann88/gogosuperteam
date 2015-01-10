7f7b984d90ede08fff1a5b45655e3c9f679c4745
Judith Rasehorn <corbit@arcor.de>
2015-01-10 14:44:16 +0100

    forum

---
+++ src/de/gogosuperteam/src/Forum.java
@@ -0,0 +1,25 @@
+package de.gogosuperteam.src;
+import java.util.ArrayList;
+public class Forum {
+
+	private int id;
+	private String name;
+	private ArrayList<Thread> threadList;
+
+	public Forum(String name) {
+		this.name = name;
+		this.threadList = new ArrayList<Thread>();
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void addThread(Thread t){
+		threadList.add(t);
+		
+	}
+	
+	
+
+}
---
+++ src/de/gogosuperteam/src/ForumDAO.java
@@ -0,0 +1,15 @@
+package de.gogosuperteam.src;
+
+public class ForumDAO {
+	
+	private int id;
+	private String name;
+	
+	public ForumDAO(String name) {
+		this.name = name;		
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+}
