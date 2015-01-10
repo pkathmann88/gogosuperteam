72c52f61eeff2e35c6da4da92534ff9aaccb6968
necrotus <ragnahr.asd@gmx.de>
2015-01-10 13:41:09 +0100

    getter and setters userdao und postdao

--- src/PostDAO.java
+++ src/PostDAO.java
@@ -4,10 +4,10 @@ import java.util.Date;
 
 public class PostDAO {
 	
-	int id ;
-	String ersteller;
-	Date erstelldatum;
-	String text ;
+	private int id ;
+	private String ersteller;
+	private Date erstelldatum;
+	private String text;
 
 	
 	
@@ -15,6 +15,50 @@ public class PostDAO {
 		
 	}
 	
+	
+	
+	public String getErsteller() {
+		return ersteller;
+	}
+
+
+
+	public void setErsteller(String ersteller) {
+		this.ersteller = ersteller;
+	}
+
+
+
+	public Date getErstelldatum() {
+		return erstelldatum;
+	}
+
+
+
+	public void setErstelldatum(Date erstelldatum) {
+		this.erstelldatum = erstelldatum;
+	}
+
+
+
+	public String getText() {
+		return text;
+	}
+
+
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
 	public void setId(int id){
 		this.id = id;
 	}
--- src/UserDAO.java
+++ src/UserDAO.java
@@ -10,4 +10,24 @@ public class UserDAO {
 	public void SetId(int id){
 		this.id = id;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
