d0fdb64fb1180d0fc8993ad1cab885eb55e94f90
necrotus <ragnahr.asd@gmx.de>
2015-01-10 12:37:08 +0100

    post class

---
+++ src/Post.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Date;
+
+public class Post {
+	
+	int id ;
+	String Ersteller;
+	Date Erstelldatum;
+	String text;
+
+	
+	
+	Post(){
+		
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+}
