8947af9cc991647721d04ea5ad49a38f07af6b4b
Judith Rasehorn <corbit@arcor.de>
2015-01-10 16:16:42 +0100

    Blür

--- src/de/gogosuperteam/src/ForumMapper.java
+++ src/de/gogosuperteam/src/ForumMapper.java
@@ -1,20 +1,19 @@
 package de.gogosuperteam.src;
-import java.util.ArrayList;
+
+import java.util.HashMap;
 
 public class ForumMapper {
-	
-	public static Forum mapToForum(ForumDAO f, ArrayList<Thread> tl) {
-		
+
+	public static Forum mapToForum(ForumDAO f, HashMap<Integer, Thread> tl) {
+
 		int idDAO = f.getId();
 		String nameDAO = f.getName();
-		
-		Forum forum = new Forum(nameDAO,tl);
+
+		Forum forum = new Forum(nameDAO, tl);
 		forum.setId(idDAO);
-		
+
 		return forum;
-		
-		
-		
+
 	}
 
 }
