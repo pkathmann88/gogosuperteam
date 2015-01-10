43849e062afbe9672e5bb4d4c256456832ccd458
Judith Rasehorn <corbit@arcor.de>
2015-01-10 12:16:39 +0100

    Forum

---
+++ src/Forum.java
@@ -0,0 +1,15 @@
+
+public class Forum {
+	
+	private int id;
+	private String name;
+	
+	public Forum(String name) {
+		this.name = name;		
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+		
+	}
+}
